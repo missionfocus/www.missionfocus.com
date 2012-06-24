0203e253c9691e74e17bf044d32033584e912246
Andrew Eick <andy@eick.us>
2012-06-24 10:59:20 -0400

    Added legacy Web site for reference

---
+++ legacy/press.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Mission Focus</title>
+<link rel="stylesheet" type="text/css" href="mainstyle.css" />
+<script src="http://www.mint.missionfoc.us/?js" type="text/javascript"></script>
+</head>
+
+<body class="inside">
+
+<div id="aboutInfo">
+		<a class="returnLink" href="index.html">RETURN ^</a>
+        <h1 class="insideTitle">ABOUT MISSION FOCUS</h1>
+        
+        <h2>MF Logo Download</h2>
+      	<p>Below are links to a variety of file types each of with different image sizes.</p>
+		<p><a href="logos/MF-Vector.zip">MF Vector Download</a></p>
+		<p><a href="logos/MF-Logo-JPG.zip">MF JPG Download</a></p>
+        <p><a href="logos/MF-Logo-PNG.zip">MF PNG Download</a>-<em>(transparent background)</em></p>
+        
+        <h2>Thing 5 Logo Download</h2>
+        <p><a href="logos/Thing5-Vector.zip">Thing 5 Vector Download</a></p>
+		<p><a href="logos/thing5logo-PNG.zip">Thing 5 JPG Download</a></p>
+        <p><a href="logos/thing5logo-JPG.zip">Thing 5 PNG Download</a>-<em>(transparent background)</em></p>
+        
+    </div>
+    
+<script type="text/javascript">
+  var _gauges = _gauges || [];
+  (function() {
+    var t   = document.createElement('script');
+    t.type  = 'text/javascript';
+    t.async = true;
+    t.id    = 'gauges-tracker';
+    t.setAttribute('data-site-id', '4f035fc9f5a1f514a1000001');
+    t.src = '//secure.gaug.es/track.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(t, s);
+  })();
+</script>
+
+
+</body>
+</html>
